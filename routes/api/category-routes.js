d2f7a80de47bcdb73f118ec4622d43691ba925ef
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-21 04:51:48 -0400

    creating get function that reads one category by its id and includes its associated Products from the database

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,19 +7,27 @@ router.get('/', async (req, res) => {
   // find all categories
   // include its associated Products
   try {
-    const LocationData = await Category.findAll({
+    const CategoryData = await Category.findAll({
       include: [{ model: Product}]
     });
-    res.status(200).json(LocationData);
+    res.status(200).json(CategoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
