45a750f5efee2ca23acbc845a8d503756abac29b
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-23 05:07:32 -0400

    creating DELETE route for deleting a tag by its id from the database

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,8 +57,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagData = await Tag.destroy({
+      where: {id: req.params.id}
+    });
+    if(!tagData){
+      res.status(404).json({message: 'No tag found with this id!'})
+    }
+    res.status(200).json({message: 'Tag deleted from database.'})
+  } catch(err){
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
